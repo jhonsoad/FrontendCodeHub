81dbb8a626d3141c6fa00bfe6ffd8e7da6b18914
Jhonatas Magalhães <jhonatasbarbosa@yahoo.com.br>
2024-07-09 18:34:15 -0300

    componentiza a apgina de acessibilidade web

--- FrontendSkillsShowcase/src/app/components/cursos/acessibilidade-web/acessibilidade-web.component.ts
+++ FrontendSkillsShowcase/src/app/components/cursos/acessibilidade-web/acessibilidade-web.component.ts
@@ -5,19 +5,24 @@ import { UtilsService } from '../../../services/utils.service';
 import { FooterA11yComponent } from "./footer-a11y/footer-a11y.component";
 import { HeaderA11yComponent } from "./header-a11y/header-a11y.component";
 import { ContactA11yComponent } from "./contact-a11y/contact-a11y.component";
+import { HighlightA11yComponent } from "./highlight-a11y/highlight-a11y.component";
+import { DifferentialA11yComponent } from "./differential-a11y/differential-a11y.component";
+import { AboutA11yComponent } from "./about-a11y/about-a11y.component";
+import { PlansA11yComponent } from "./plans-a11y/plans-a11y.component";
+import { BlogA11yComponent } from "./blog-a11y/blog-a11y.component";
+import { HighlightsA11yComponent } from "./highlights-a11y/highlights-a11y.component";
+import { InstitutionalA11yComponent } from "./institutional-a11y/institutional-a11y.component";
 
 @Component({
   selector: 'app-acessibilidade-web',
   standalone: true,
-  imports: [NgClass, FooterA11yComponent, HeaderA11yComponent, ContactA11yComponent],
+  imports: [NgClass, FooterA11yComponent, HeaderA11yComponent, ContactA11yComponent, HighlightA11yComponent, DifferentialA11yComponent, AboutA11yComponent, PlansA11yComponent, BlogA11yComponent, HighlightsA11yComponent, InstitutionalA11yComponent],
   templateUrl: './acessibilidade-web.component.html',
   styleUrl: './acessibilidade-web.component.scss'
 })
 export class AcessibilidadeWebComponent implements OnInit {
 
   contatoApeperia: string = 'contato@apeperia.com';
-  botaoAtivo: number = 0;
-  modal: boolean = false;
 
   constructor(
     private titleService: Title,
@@ -38,18 +43,6 @@ export class AcessibilidadeWebComponent implements OnInit {
 
   adicionaLinksTags(): void {
     this.utilsService.adicionaLinkTag('stylesheet', 'https://fonts.googleapis.com/css?family=Montserrat:300,400,700|Open+Sans:300,400,700');
-  }
-
-  carosselJs(noticia: number): void {
-    this.botaoAtivo = noticia;
-  }
-
-  abriModal(): void {
-    this.modal = true;
-  }
-
-  fechaModal(): void {
-    this.modal = false;
   }
 
 }
