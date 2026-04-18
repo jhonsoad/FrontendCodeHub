f9f296f21cd82061d7f5126abe7f4c38493f1e7f
Jhon <jhonatasbarbosa@yahoo.com.br>
2026-04-18 01:36:00 -0300

    test: corrige testes unitarios

--- src/app/pages/projects/projects.component.spec.ts
+++ src/app/pages/projects/projects.component.spec.ts
@@ -1,14 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ProjectsComponent } from './projects.component';
+import { ActivatedRoute } from '@angular/router';
 
 describe('ProjectsComponent', () => {
   let component: ProjectsComponent;
   let fixture: ComponentFixture<ProjectsComponent>;
 
   beforeEach(async () => {
+    const activatedRouteSpy = {
+      snapshot: { params: {} }
+    };
+
     await TestBed.configureTestingModule({
-      imports: [ProjectsComponent]
+      imports: [ProjectsComponent],
+      providers: [
+        { provide: ActivatedRoute, useValue: activatedRouteSpy }
+      ]
     })
     .compileComponents();
 
